d9575b2e7e2f1c9d1158dc6b6c7098505226f925
axu <axu@iastate.edu>
2022-05-20 15:21:36 -0500

    Q13 sol

--- LeetCode/src/Roman2Int.java
+++ LeetCode/src/Roman2Int.java
@@ -1,13 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
-
+// Q13 Roman to Integer
 class Roman2Int{
-    private Map<Character, Integer> map = new HashMap();
     public int romanToInt(String s) {
-        return 0;
-    }
-
-    public void Roman2IntMap(){
+        Map<Character, Integer> map = new HashMap();
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -15,5 +11,20 @@ class Roman2Int{
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
+        int result = 0;
+        char prev = ' ';
+        for(int i = s.length()-1; i>=0;i--){
+            char curr = s.charAt(i);
+            result = result + map.get(curr);
+            if((prev == 'V' || prev == 'X') && curr == 'I'){
+                result = result - 2;
+            }else if((prev == 'L' || prev == 'C') && curr == 'X'){
+                result = result - 20;
+            }else if((prev == 'D' || prev == 'M') && curr == 'C'){
+                result = result - 200;
+            }
+            prev = curr;
+        }
+        return result;
     }
 }
