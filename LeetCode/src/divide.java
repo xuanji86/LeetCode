058fde2f8ac0cb160048af69e923ef4942cfe12f
axu <axu@iastate.edu>
2022-06-17 15:11:49 -0500

    Q29

---
+++ LeetCode/src/divide.java
@@ -0,0 +1,44 @@
+public class divide {
+    public int divide(int dividend, int divisor) {
+    
+        long res = solve(dividend, divisor);
+        
+        return res > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)res;
+    }
+    
+    public long solve(long dividend, long divisor){
+        
+        if(dividend < 0 || divisor < 0){
+            
+            if(dividend < 0 && divisor < 0)
+                return  solve(Math.abs(dividend),  Math.abs(divisor));
+            
+            long ans = solve(Math.abs(dividend),  Math.abs(divisor));
+            return 0 - ans;
+        }
+        
+        if(dividend < divisor)
+            return 0;
+        
+        if(divisor == 1)
+            return dividend;
+        
+        long multiples = 1;
+        long sum = divisor;
+        
+        while(sum < dividend){
+            multiples += multiples;
+            sum += sum;
+        }
+        
+        if(sum == dividend){
+            return multiples;
+        }
+        
+        while(sum > dividend){
+            sum -= divisor;
+            multiples -= 1;
+        }
+        return multiples;
+    }
+}
