b6198e7ef50bb3a274309bc612848759a86df3e4
axu <axu@iastate.edu>
2022-06-17 17:41:35 -0500

    Q19

--- LeetCode/src/addTwoNumber.java
+++ LeetCode/src/addTwoNumber.java
@@ -1,18 +1,16 @@
-
-public class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { 
-        this.val = val; 
-    }
-     ListNode(int val, ListNode next) {
-        this.val = val; 
-        this.next = next; 
-    }
- }
- 
 public class addTwoNumber{
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { 
+           this.val = val; 
+       }
+        ListNode(int val, ListNode next) {
+           this.val = val; 
+           this.next = next; 
+       }
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode result = new ListNode();
         ListNode current = result;
